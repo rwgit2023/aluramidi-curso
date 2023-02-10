c2124e8eca7593944f963dc8d9c1e2126bf86f42
rwgit2023 <ronangit2023@hotmail.com>
2023-02-10 16:29:39 -0300

    Second Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.querySelector('.tecla_pom') A palavra document é uma palavra reservada no JS
+//   (query.) -- O . em JS significa que vamos abrir algo em JS
+//   O querySelector é utilizado quando vamos buscar algo 
+
+
+document.querySelector('.tecla_pom')
+
+document.querySelector('#som_tecla_pom').play();

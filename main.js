e6519ca2e7604a2304c859f7fa824ba4339fd3dd
rwgit2023 <ronangit2023@hotmail.com>
2023-02-13 16:54:20 -0300

    Fourth Commit

--- main.js
+++ main.js
@@ -3,24 +3,44 @@
 //   O querySelector é utilizado quando vamos buscar algo 
 
 
-function tocaSomPom() {
-
-    document.querySelector('#som_tecla_pom').play();
-}
-
-
+// function tocaSomPom() {
+// 
+    // document.querySelector('#som_tecla_pom').play();
+// }
+// 
 
 // document.querySelector('.tecla_pom').onclick = tocaSomPom;       pega apenas 1 tecla
 
 
+// const listaDeTeclas = document.querySelectorAll('.tecla');  // pega todas as classes (botoes)
+
+
+// for(let i = 0; i < listaDeTeclas.length; i++){
+// 
+    // listaDeTeclas[i].onclick = tocaSomPom
+    // console.log(i)
+// }
+// 
+// 
 
 
 
+function tocaSom(idElementoAudioAudio){
 
-const listaDeTeclas = document.querySelectorAll('.tecla');  // pega todas as classes (botoes)
-
-
-
-
+    document.querySelector(idElementoAudioAudio).play();
+}
 
 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+while(contador < listaDeTeclas.length){
+
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador +=1
+
+    console.log(contador);
+}

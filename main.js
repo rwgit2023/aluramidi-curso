6d17c95731f8f998c07a0dfae1d9aafe7d6df6bb
rwgit2023 <ronangit2023@hotmail.com>
2023-02-15 17:02:12 -0300

    Sixth Commit

--- main.js
+++ main.js
@@ -15,13 +15,18 @@
 // const listaDeTeclas = document.querySelectorAll('.tecla');  // pega todas as classes (botoes)
 
 
-// for(let i = 0; i < listaDeTeclas.length; i++){
+// while(contador < listaDeTeclas.length){
 // 
-    // listaDeTeclas[i].onclick = tocaSomPom
-    // console.log(i)
+    // const tecla = listaDeTeclas[contador];
+    // const instrumento = tecla.classList[1]; // pega o nome faltante na string
+    // const idAudio = `#som_${instrumento}`;        // soma oque euu tinha com as novas
+    // tecla.onclick = function(){
+        // tocaSom (idAudio)
+    // }
+    // contador +=1
 // }
-// 
-// 
+
+
 
 
 // Funcao com Parametro de entrada (toca o som)
@@ -30,28 +35,22 @@ function tocaSom(idElementoAudioAudio){
     document.querySelector(idElementoAudioAudio).play();
 }
 
-
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-
-while(contador < listaDeTeclas.length){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1]; // pega o nome faltante na string
-
     //Template String
     const idAudio = `#som_${instrumento}`;        // soma oque euu tinha com as novas
-
-    // console.log(idAudio);
-    
     // listaDeTeclas[contador].onclick = tocaSom;
     tecla.onclick = function(){
         tocaSom (idAudio)
     }
-    contador +=1
+      
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa')
+    }
+} 
 
-    // console.log(contador);
-}
+
